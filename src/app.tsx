bab8d9b9ce716e384c2232c2ea6f08091241ddff
Money <manhtien310701@gmail.com>
2026-06-24 14:17:47 +0800

    feat: wire up ShellLayout and new tab routes

--- src/app.tsx
+++ src/app.tsx
@@ -1,17 +1,28 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import React, { Suspense } from 'react'
-import { Route } from 'react-router-dom'
+import { Outlet, Route } from 'react-router-dom'
 import { AnimationRoutes, App, SnackbarProvider, ZMPRouter } from 'zmp-ui'
 
-import HomePage from './pages/home'
 import ChatPage from './pages/chat'
-import SettingsPage from './pages/settings'
+import SurveyPage from './pages/survey'
+import ReviewPage from './pages/review'
+import UserPage from './pages/user'
+import { TabBar } from './components/tab-bar'
 
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: { retry: 2 },
   },
 })
+
+function ShellLayout() {
+  return (
+    <div className="min-h-screen pb-16">
+      <Outlet />
+      <TabBar />
+    </div>
+  );
+}
 
 const MyApp = () => {
   return (
@@ -21,9 +32,12 @@ const MyApp = () => {
           <SnackbarProvider>
             <ZMPRouter>
               <AnimationRoutes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/chat" element={<ChatPage />} />
-                <Route path="/settings" element={<SettingsPage />} />
+                <Route element={<ShellLayout />}>
+                  <Route path="/" element={<ChatPage />} />
+                  <Route path="/survey" element={<SurveyPage />} />
+                  <Route path="/review" element={<ReviewPage />} />
+                  <Route path="/user" element={<UserPage />} />
+                </Route>
               </AnimationRoutes>
             </ZMPRouter>
           </SnackbarProvider>
