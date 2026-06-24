bc99e539a7b2907a20dd1410f304f93612ac857c
Money <manhtien310701@gmail.com>
2026-06-24 11:08:46 +0800

    chore: add katex stylesheet, remove swiper import

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,7 @@
-// Import React and ReactDOM
 import 'zmp-ui/zaui.css'
 import './css/app.scss'
 import './polyfills'
 import 'dayjs/locale/vi'
-import 'swiper/css'
 
 import dayjs from 'dayjs'
 import localizedFormat from 'dayjs/plugin/localizedFormat'
@@ -11,13 +9,11 @@ import relativeTime from 'dayjs/plugin/relativeTime'
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 
-// Import App Component
 import App from './app'
 
 dayjs.locale('vi-VN')
 dayjs.extend(localizedFormat)
 dayjs.extend(relativeTime)
 
-// Mount React App
 const root = createRoot(document.getElementById('app')!)
 root.render(React.createElement(App))
